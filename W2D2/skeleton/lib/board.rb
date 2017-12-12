8405dfa8442350fbee3d42db50cb1515d3f0108d
Neil <neilrichardson@outlook.com>
2017-12-12 08:57:53 -0800

    added cups && :stones

--- W2D2/skeleton/lib/board.rb
+++ W2D2/skeleton/lib/board.rb
@@ -2,13 +2,24 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = Array.new(14) {Array.new}
+    place_stones
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, idx|
+      next if idx == 6 || idx == 13
+      4.times do
+        cup << :stone
+      end
+    end
   end
 
   def valid_move?(start_pos)
+    if start_pos < 0 || start_pos > 13 || @stone[start_pos].empty?
+      raise "Invalid starting cup"
+    end
+    true
   end
 
   def make_move(start_pos, current_player_name)
