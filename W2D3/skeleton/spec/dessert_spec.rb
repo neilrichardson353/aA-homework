fd62dbf4407f168cfd094e8cdfdac64c9f21ad7f
Neil <neilrichardson@outlook.com>
2017-12-13 08:31:59 -0800

    Compared my answers to the solutions

--- W2D3/skeleton/spec/dessert_spec.rb
+++ W2D3/skeleton/spec/dessert_spec.rb
@@ -6,39 +6,60 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
+  let(:chef) { double("chef", name: 'Neil') }
+  let(:brownie) {Dessert.new("dessert", 3, chef) }
 
   describe "#initialize" do
-    it "sets a type"
-      expect(self.initialize('dessert',3, "Neil")).to eq(@type == 'dessert')
-    it "sets a quantity"
-      expect(self.initialize('dessert',3, "Neil")).to eq(@quantity == 3)
-    it "starts ingredients as an empty array"
-      expect(self.initialize('dessert',3, "Neil")).to eq(@ingredients == [])
-    it "raises an argument error when given a non-integer quantity"
-      expect { self.initialize('dessert',"3", "Neil") }.to raise_error(ArgumentError)
+    it "sets a type" do
+      expect(brownie.type).to eq('dessert')
+    end
+
+    it "sets a quantity" do
+      expect(brownie.quantity).to eq(3)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(brownie.quantity).to eq([])
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Desert.new.('salt', 'lots', neil) }.to raise_error(ArgumentError)
+    end
   end
-=begin
+
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      brownie.add_ingredient('chocolate')
+    expect(brownie.ingredients).to include('chocolate')
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      ingredient = ["salt", "chocolate", 'egg']
+
+      ingredient.each do |el|
+        brownie.add_ingredient(ingredient)
+      end
+      expect(brownie.ingredients).to eq(ingredient)
+      brownie.mix!
+      expect(brownie.ingredients).to eq(ingredients)
+      expect(brownie.ingredients.sort).to eq(ingredients.sort)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
-
-    it "raises an error if the amount is greater than the quantity"
+    it "subtracts an amount from the quantity" do
+      brownie.eat(1)
+    expect(brownie.quantity).to eq(2)
+    it "raises an error if the amount is greater than the quantity" do
+    expect {self.eat('nothing') }.to raise(ArgumentError)
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
   end
-=end
 end
